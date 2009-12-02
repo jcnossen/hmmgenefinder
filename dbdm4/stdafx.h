e6757da38373f51e99f2772deb8e41fd5fbd1b3e
unknown <jelmer@.(none)>
2009-12-02 13:01:31 +0100

    include all ghmm headers

--- dbdm4/stdafx.h
+++ dbdm4/stdafx.h
@@ -7,4 +7,40 @@
 
 #include <stdio.h>
 #include "ghmm/ghmm.h"
-#include 
+#include "ghmm/model.h"
+#include "ghmm/modelutil.h"
+#include "ghmm/foba.h"
+#include "ghmm/gauss_tail.h"
+#include "ghmm/gradescent.h"
+#include "ghmm/kbest.h"
+#include "ghmm/kbestbasics.h"
+#include "ghmm/linkedlist.h"
+#include "ghmm/matrix.h"
+#include "ghmm/mes.h"
+#include "ghmm/mprintf.h"
+#include "ghmm/pmodel.h"
+#include "ghmm/psequence.h"
+#include "ghmm/pviterbi.h"
+#include "ghmm/pviterbi_propagate.h"
+#include "ghmm/randvar.h"
+#include "ghmm/reestimate.h"
+#include "ghmm/discrime.h"
+#include "ghmm/rng.h"
+#include "ghmm/root_finder.h"
+#include "ghmm/scluster.h"
+#include "ghmm/sdmodel.h"
+#include "ghmm/sdfoba.h"
+#include "ghmm/sequence.h"
+#include "ghmm/sfoba.h"
+#include "ghmm/sgenerate.h"
+#include "ghmm/smap_classify.h"
+#include "ghmm/smixturehmm.h"
+#include "ghmm/smodel.h"
+#include "ghmm/sreestimate.h"
+#include "ghmm/sviterbi.h"
+#include "ghmm/vector.h"
+#include "ghmm/viterbi.h"
+
+
+
+
