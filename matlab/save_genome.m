396b3596101097e082cabd3e2b7c12b8b0ea4195
unknown <jc@.(none)>
2010-01-15 21:32:21 +0100

    * ecoli genome exported from matlab * save_genome working * CfgParser

--- matlab/save_genome.m
+++ matlab/save_genome.m
@@ -1,60 +1,117 @@
 % Downloads genome from GenBank and stores it on disk in a text format
 % (one that is a little easier to parse than GBK)
-function save_genome(name, filename)
-    
-    g = get_bank_file(name);
-    f = featuresparse(g.Features);
-    
-    file = fopen(filename, 'w');
-    
-    write_named_value(g
-    
-    for i=1:length(f) 
+function save_genome(data, filename)
+    if ischar(data)
+        g = get_bank_file(name);
+        f = featuresparse(g.Features);
+    else
+        g = data{1};
+        f = data{2};
     end
     
+    indents = 0;
     
-    fclose(file);
+    % write sequence data to a seperate flat text file
+    seqfilename = [filename '.seq'];
+    seqfile = fopen(seqfilename, 'w');
+    fprintf(seqfile, g.Sequence);
+    fclose(seqfile);
 
-    function write_named_value(v, name)
-        fprintf(file,'%s = ', v);
-        write_value(v);
+    file = fopen(filename, 'w');
+    write_gbk(g);
+    write_features(f);
+    fclose(file);
+    
+
+    function write_field(sv, name)
+        fprintf(file,'%s = ', name);
+        write_value(sv.(name));
         fprintf(file, '\n');
+    end
+
+    function write_features(f)
+        fprintf(file, 'features = ');
+        write_struct(f, {'tRNA', 'CDS', 'rRNA', 'misc_RNA', 'misc_feature'});
+    end
+
+    function write_gbk(g)
+        write_field(g, 'LocusName');
+        write_field(g, 'LocusSequenceLength');
+        write_field(g, 'LocusTopology');
+        write_field(g, 'LocusMoleculeType');
+        write_field(g, 'LocusGenBankDivision');
+        write_field(g, 'LocusModificationDate');
+        write_field(g, 'Definition');
+        write_field(g, 'Source');
+        tabs(); fprintf(file, 'SequenceFile = %s\n', seqfilename);
     end
     
     function write_value(v)
-        
-        if isstruct(v)
-            write_struct(v);
-        elseif ischar(v)
-            fprintf(file, '"%s"', v);
-        elseif isnumeric(v) | iscell(v)
-            write_array(v);
-        elseif isscalar(v)
-            fprintf(file,'%f',v); 
+        [r k]=size(v);
+        if k>1
+            if iscell(v)
+                error('no cell array support implemented');
+            elseif r>1
+                error('no matrix writing implemented');
+            elseif ischar(v)
+                fprintf(file, '"%s"', v);
+            else
+                write_array(v)
+            end
         else
-            error(['unknown value type: ' v]);
+            if isstruct(v)
+                write_struct(v);
+            elseif ischar(v) % single char
+                fprintf(file, '"%s"', v);
+            elseif isscalar(v)
+                fprintf(file,'%f',v); 
+            else
+                error(['unknown value type: ' v]);
+            end
         end
     end
 
     function write_array(v)
-        fprintf(file,'{');
-        for i=1:length(v)
-            write_value(v(i));
+        [rows cols]=size(v);
+        if (rows>1)
+            error('only row vectors (#rows=1)');
         end
-        fprintf(file, '}\n');
+        fprintf(file,'{\n');
+        indents=indents+1;
+        for i=1:cols
+            tabs();
+            write_value(v(i));
+            fprintf(file,'\n');
+        end
+        indents=indents-1;
+        tabs(); fprintf(file, '}\n');
     end
    
-    function write_struct(s)
+    function write_struct(s, fields)
         fprintf(file,'{\n');
-        names = fieldnames(s);
-        for i=1:length(names)
-            v = s.(names{i});
-            fprintf(file, '%s = ', names{i});
-            fprintf('writing %s\n', names{i});
+        if nargin == 1
+            fields = fieldnames(s);
+        end
+        indents=indents+1;
+        for i=1:length(fields)
+            v = s.(fields{i});
+            if length(v)==0
+                continue
+            end
+            tabs();
+            fprintf(file, '%s = ', fields{i});
+            %fprintf('writing %s\n', fields{i});
             write_value(v);
             fprintf(file, '\n');
         end
-        fprintf(file,'}\n');
+        indents=indents-1;
+        tabs(); fprintf(file,'}\n');
+    end
+    
+    function tabs()
+        for i=1:indents
+            fprintf(file,'\t');
+        end
     end
     
 end
